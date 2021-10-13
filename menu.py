0616c6833822ea4afb997ea7f6793cbb3bf1e76e
PhilippeRambaud <phiram2306@gmail.com>
2021-10-13 15:57:23 +0200

    basic menu

--- menu.py
+++ menu.py
@@ -1,11 +1,10 @@
 import pygame, sys
 from pygame.locals import *
+from classes import Button
 
 main_clock = pygame.time.Clock()
 
 pygame.init()
-pygame.font.init()
-font = pygame.font.SysFont(None, 20)
 
 width = 500
 height = 500
@@ -17,30 +16,6 @@ click = False
 
 bt_w = 100
 bt_h = 50
-
-class Button:
-    def __init__(self, text, x, y, width, height, color):
-        self.text = text
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.color = color
-
-    def draw(self, win):
-        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
-        text = font.render(self.text, 1, (0, 0, 0))
-        win.blit(text, (self.x + round(self.width/2) - round(text.get_width()/2),
-                 self.y + round(self.height/2) - round(text.get_height()/2)))
-
-    def click(self, pos):
-        x1 = pos[0]
-        y1 = pos[1]
-        # check if coordinates (pos) of mouse are actually on our button
-        if self.x <= x1 <= self.x + self.width and self.y <= y1 <= self.y + self.height:
-            return True
-        else:
-            return False
 
 
 def main_menu():
